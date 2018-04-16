cf4634624c472f45ce55601532a4cdc827f78274
MediaPlayers <38300962+MediaPlayers@users.noreply.github.com>
2018-04-15 21:51:53 -0500

    explained .getSelectionModel.getSelectedItems
    -sheilf

--- SimpleListView.java
+++ SimpleListView.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Basic functionality using ListView */ 
+
 package simplelistview;
 
 
@@ -71,6 +68,7 @@ public class SimpleListView extends Application {
 
         System.out.println(songSelected);
         
+        //Songs gets the list of strings in listViewComponent
     }
     /**
      * @param args the command line arguments
